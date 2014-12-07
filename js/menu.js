e5ff17939bb3fd5afdd8a04a7c2250fec18c95ba
Ruslan Kozlov <ruslan@pieoneers.com>
2014-12-08 00:02:34 +0700

    newyear page

---
+++ js/menu.js
@@ -0,0 +1,73 @@
+$(function () {
+  $('.to_consultation_order').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#consultation_order').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_ny_bowl').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#ny_bowl').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_ready_sets').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#ready_sets').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_ny_bowl_order').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#ny_bowl_order').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_ready_sets_order').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#ready_sets_order').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_feedbacks').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#feedbacks').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_team').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#team').offset().top - 50
+    }, 250);
+  });
+
+  $('.to_contacts').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#contacts').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_new_year_cards').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#new_year_cards').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_decor_set').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#decor_set').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_top').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#top').offset().top - 75
+    }, 250);
+  });
+
+  $('.to_contacts').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#contacts').offset().top - 50
+    }, 250);
+  });
+});
