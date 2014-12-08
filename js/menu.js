5a52b40493fca407789ecd4cb8291016a86ac62f
Ruslan Kozlov <ruslan@pieoneers.com>
2014-12-08 21:22:23 +0700

    mobile fix

--- js/menu.js
+++ js/menu.js
@@ -1,73 +1,76 @@
 $(function () {
+var time = 1000;
+var offset = 75;
+
   $('.to_consultation_order').click(function (){
     $('html, body').animate({
-      scrollTop: $('#consultation_order').offset().top - 75
-    }, 250);
+      scrollTop: $('#consultation_order').offset().top - offset
+    }, time);
   });
 
   $('.to_ny_bowl').click(function (){
     $('html, body').animate({
-      scrollTop: $('#ny_bowl').offset().top - 75
-    }, 250);
+      scrollTop: $('#ny_bowl').offset().top - offset
+    }, time);
   });
 
   $('.to_ready_sets').click(function (){
     $('html, body').animate({
-      scrollTop: $('#ready_sets').offset().top - 75
-    }, 250);
+      scrollTop: $('#ready_sets').offset().top - offset
+    }, time);
   });
 
   $('.to_ny_bowl_order').click(function (){
     $('html, body').animate({
-      scrollTop: $('#ny_bowl_order').offset().top - 75
-    }, 250);
+      scrollTop: $('#ny_bowl_order').offset().top - offset
+    }, time);
   });
 
   $('.to_ready_sets_order').click(function (){
     $('html, body').animate({
-      scrollTop: $('#ready_sets_order').offset().top - 75
-    }, 250);
+      scrollTop: $('#ready_sets_order').offset().top - offset
+    }, time);
   });
 
   $('.to_feedbacks').click(function (){
     $('html, body').animate({
-      scrollTop: $('#feedbacks').offset().top - 75
-    }, 250);
+      scrollTop: $('#feedbacks').offset().top - offset
+    }, time);
   });
 
   $('.to_team').click(function (){
     $('html, body').animate({
       scrollTop: $('#team').offset().top - 50
-    }, 250);
+    }, time);
   });
 
   $('.to_contacts').click(function (){
     $('html, body').animate({
-      scrollTop: $('#contacts').offset().top - 75
-    }, 250);
+      scrollTop: $('#contacts').offset().top - offset
+    }, time);
   });
 
   $('.to_new_year_cards').click(function (){
     $('html, body').animate({
-      scrollTop: $('#new_year_cards').offset().top - 75
-    }, 250);
+      scrollTop: $('#new_year_cards').offset().top - offset
+    }, time);
   });
 
   $('.to_decor_set').click(function (){
     $('html, body').animate({
-      scrollTop: $('#decor_set').offset().top - 75
-    }, 250);
+      scrollTop: $('#decor_set').offset().top - offset
+    }, time);
   });
 
   $('.to_top').click(function (){
     $('html, body').animate({
-      scrollTop: $('#top').offset().top - 75
-    }, 250);
+      scrollTop: $('#top').offset().top - offset
+    }, time);
   });
 
   $('.to_contacts').click(function (){
     $('html, body').animate({
       scrollTop: $('#contacts').offset().top - 50
-    }, 250);
+    }, time);
   });
 });
